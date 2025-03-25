28fac552d4f164201c0e090d4198b15ceefcae4a
zelie-delhotal <gatien.delhotal@orange.fr>
2025-03-25 01:08:30 +0100

    memmove first try

--- ft_memmove.c
+++ ft_memmove.c
@@ -14,5 +14,22 @@
 
 void	*ft_memmove(void *dest, const void *src, size_t n)
 {
-	char	*buffer;
+	char	*d_cast;
+	char	*s_cast;
+	size_t	i;
+
+	d_cast = (char *) dest;
+	s_cast = (char *) src;
+	i = -1;
+	if (s_cast < d_cast)
+	{
+		while (++i < n)
+			d_cast[n - i - 1] = s_cast[n - i - 1];
+	}
+	else
+	{
+		while (++i < n)
+			d_cast[i] = s_cast[i];
+	}
+	return (dest);
 }
